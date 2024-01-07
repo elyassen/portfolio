606d9ddf583dd24ba85e8c7bee6f03bd5e097677
yaseen <yaseensen9@gmail.com>
2024-01-07 16:20:23 +0530

    ini

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+    <h4>Work</h4>
+    <h4>About</h4>
+    <h4>Contact</h4>
+    <div className='last'>
+    instage
+    insta
+    </div>
+    
+      
+    </div>
+  )
+}
+
+export default Header
